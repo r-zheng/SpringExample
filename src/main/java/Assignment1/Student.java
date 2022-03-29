73aedc1079a059689263a630b4c7b2d58d7f46c6
r-zheng <raymond.zheng@stonybrook.edu>
2022-03-29 11:47:03 -0400

    Assignment1

---
+++ src/main/java/Assignment1/Student.java
@@ -0,0 +1,52 @@
+package Assignment1;
+
+import java.util.List;
+
+public class Student {
+    private int id;
+    private String name;
+    private List<Phone> ph;
+    private Address address;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Phone> getPh() {
+        return ph;
+    }
+
+    public void setPh(List<Phone> ph) {
+        this.ph = ph;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", ph=" + ph +
+                ", address=" + address +
+                '}';
+    }
+}
