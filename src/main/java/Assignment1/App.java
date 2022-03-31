c5af3aae28b8b8384bbfd330921627975150ddb2
r-zheng <raymond.zheng@stonybrook.edu>
2022-03-31 13:34:16 -0400

    assignment 2

--- src/main/java/Assignment1/App.java
+++ src/main/java/Assignment1/App.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("Assignment1Spring.xml");
         System.out.println(context.getBean("student").toString());
     }
 }
